582f608196d6d6ac2fd954a90f315b327f8d7623
Elisha Ricketts <ecricketts@uncg.edu>
2025-10-16 18:42:00 -0400

    Added: backend-api.

---
+++ backend-api/src/main/java/com/csc340/spartanfitness/SpartanfitnessApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.spartanfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartanfitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartanfitnessApplication.class, args);
+	}
+
+}
---
+++ backend-api/src/test/java/com/csc340/spartanfitness/SpartanfitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc340.spartanfitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartanfitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
